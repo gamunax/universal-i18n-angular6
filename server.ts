e85d5f64c7e1d8f41bcc7a5a2bbdc301f987cf00
José Sanchez <jsanchez@tcruzdelsur.tcs>
2018-07-02 17:15:32 -0500

    borrando comentarios obsoletos

--- server.ts
+++ server.ts
@@ -1,55 +1,10 @@
-/*import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
-
-import { enableProdMode } from '@angular/core';
-
-import * as express from 'express';
-import { join } from 'path';
-
-enableProdMode();
-
-const app = express();
-
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist');
-
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
-
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-
-app.engine('html', ngExpressEngine({
-	bootstrap: AppServerModuleNgFactory,
-	providers: [
-		provideModuleMap(LAZY_MODULE_MAP)
-	]
-}));
-
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser'));
-
-const languages = ['es', 'en'];
-for (const language of languages) {
-	app.get('/' + language + '/*.*', express.static(join(DIST_FOLDER, 'browser', language)));
-	app.get('/' + language + '/*', (req, res) => {
-		res.render(join(DIST_FOLDER, 'browser', language,  'index.html'), { req });
-	});
-}
-
-app.listen(PORT, () => {
-	console.log(`Node server listening on http://localhost:${PORT}`);
-});
-*/
-
 import 'reflect-metadata';
 import 'zone.js/dist/zone-node';
-import { renderModuleFactory } from '@angular/platform-server';
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import { enableProdMode } from '@angular/core';
 import * as express from 'express';
 import { join } from 'path';
-import { readFileSync } from 'fs';
 
 enableProdMode();
 
